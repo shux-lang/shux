c5834f05ed774471a3822fc0a1c5aa7303e43832
Contentmaudlin <ussaklimert@gmail.com>
2017-03-25 22:35:20 -0400

    semant beginning work

--- src/backend/semant.ml
+++ src/backend/semant.ml
@@ -1,3 +1,11 @@
 (* let's do some very basic checking here *) 
-let check ast = 
-       ast 
+
+open Ast
+
+module GlobalMap = Map.Make(String)
+
+let check (ns, globals, functions) = 
+				(* Checking functions *)
+				if not List.exists (fun fd -> fd.fname = "main" && fd.fn_typ = Int) functions
+				then raise (Failure ("no main method given")) else ();
+				(ns, globals, functions) 
