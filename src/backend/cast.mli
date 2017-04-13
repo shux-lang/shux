65cfde5e580a6288c4401e9851a59f388f7ada8e
j-hui <j.hui@columbia.edu>
2017-04-13 02:55:50 -0400

    removed assignment as an operator from frontend

--- src/backend/cast.mli
+++ src/backend/cast.mli
@@ -3,52 +3,53 @@
 
 (* I am also going to use just regular names...
    unsure how OCaml handles name type conflicts *) 
-type typ = 
- | Int
- | Float
- | String
- | Bool
- | Struct of string 
- | Array of typ
- | Vector of int
- | Void
-
-type mut = 
- | Mutable
- | Immutable
-
-type bind = Bind of mut * typ * string
-
-(* Struct of arrays/circbuffs *)
-
-type kn = {
-	fname		 : string;
-	ret_typ	 : typ;
-	formals	 : bind list;
-	body		 : stmt	list;
-	ret_expr : expr;
-}
-
-type vkn = {
-  fname		 : string
-	formals	 : bind list;
-	body		 : stmt list;
-}
 
 (* Generator *) 
+(*
 type buffer = typ * int
 
 type gn_struct = buffer list
 
 type gn = Gn of gn_struct * kn
+*)
 
+type ctyp = styp
 
-type bin_op =
-  | Add | Sub | Mul | Div | Mod | Exp
-  | Asn
-  | Eq | Lt | Gt | Neq | Leq | Geq
-  | LogAnd | LogOr
-  | Index
-  | Access
+type cbind = sbind
 
+type cbin_op =
+  | CAddi | CSubi | CMuli | CDivi | CMod | CExpi
+  | CAddf | CSubf | CMulf | CDfvf | CMod | CExpf
+  | CAsn
+  | CEqi | CLti | CGti | CNeqi | CLeqi | Geqi
+  | CEqf | CLtf | CGtf | CNeqf | CLeqf | Geqf
+  | CLogAnd | CLogOr
+  | CIndex
+  | CAccess
 
+type cun_op =
+  | CLogNot | CNegi | CNegf
+
+type clit =
+  | CLitInt of int
+  | CLitFloat of float
+  | CLitBool of bool
+  | CLitStr of string
+  | CLitArray of cexpr list
+  | CLitStruct of cstruct_field list
+
+and cstruct_field = CStructField of string * cexpr
+
+and cexpr =
+  | CLit of ctyp * clit
+  | CId of ctyp * string
+  | CBinop of ctyp * cexpr * cbin_op * cexper
+  | 
+
+type cdecl =
+  | CFnDecl of cfn_decl
+  | CStructDef of cstruct_def
+  | CConstDecl of cbind * cexpr
+  | SExternDecl of sextern_decl
+
+type cprogram = cdecl list
