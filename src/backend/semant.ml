e9cd7413df9364b2099b4ec541b91d9df2cf5e63
Contentmaudlin <ussaklimert@gmail.com>
2017-04-18 04:07:01 +0000

    setting up semant translation

--- src/backend/semant.ml
+++ src/backend/semant.ml
@@ -1,12 +1,59 @@
-(* let's do some very basic checking here *) 
-
 open Ast
+open Sast
 open Astprint
 
-module GlobalMap = Map.Make(String)
+(* map variable to type 
+	or stack of types if overridden *) 
+module VarMap = Map.Make(struct
+				type t = string
+				let compare x y = Pervasives.compare x y
+		end) 
 
+(* Set of var names *) 
+module VarSet = Set.Make(struct
+			type t = string
+			let compare x y = Pervasives.compare x y
+		end)
+
+(* represent typed variables with same name *)
+type var = {
+	id : string; 
+	obj_type : Ast.typ;
+}
+
+type trans_env = {
+		(*  a stack of objects mapped to a name *)
+		scope: var list VarMap.t;
+	
+		
+		(* return type of block *)
+		ret_type : Sast.typ;
+}
+
+(* TODO: take in a list of globals and create a trans_env *) 
+let create_new_env decls = decls
+
+(*TODO: Flatten namespaces
+	- create new Ast.program of flattened ns and return *)
+let flatten_namespaces ns = ns 
+
+(*TODO: *) 
+let check_globals a = a
+
+(* main type checking goes on here *) 
+let check_functions run_env = run_env
+
+(* entry point *) 
 let check (ns, globals, functions) = 
-				(* Checking functions *)
+	let flat_ns = flatten_namespaces ns in
+	let global_env = check_globals (globals @ (snd flat_ns)) in
+	let start_env = create_new_env global_env in 
+	check_functions global_env
+
+
+(*				(* Checking functions *)
 				if not (List.exists (fun fd -> (fd.fname = "main" && (Astprint.string_of_typ fd.ret_typ) = "int")) functions)
 				then raise (Failure ("no main method given")) else ();
-				(ns, globals, functions) 
+				(ns, globals, functions)
+
+*)
--- src/frontend/ast.mli
+++ src/frontend/ast.mli
@@ -91,7 +91,7 @@ type let_decl =
 
 type ns_decl = {
   nname     : string;
-  nbody     : program;
+  nbody     : let_decl list * fn_decl list;
 }
 
 and program = ns_decl list * let_decl list * fn_decl list
