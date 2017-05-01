34ad417e5bfe40dfb0da3cefcd6a7f6b93e8ff8e
Lucas Schuermann <lvs2124@columbia.edu>
2017-05-01 23:17:07 +0000

    more scripting fun

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,16 +2,22 @@
 
 compiler=./shuxc
 runtime=lli
+test_ext=".shux"
 obj_ext=".ll"
 out_ext=".out"
 
-echo "Running all tests sequentially..."
-test_names=`ls *.shux`
-for test in $test_names
-do
-    echo 'running '$test
-    test_obj=$test$obj_ext
-    test_out=$test$out_ext
-    $compiler $test > $test_obj
-    $runtime $test_obj > $test_out 
-done
+if [ $# -eq 1 ]; then
+    echo "Single test running not yet supported"
+else
+    echo "Running all tests sequentially..."
+    test_names=`ls *.shux`
+    for test in $test_names
+    do
+	test=`echo "$test" | cut -d'.' -f1`
+	echo 'running '$test
+	test_obj=$test$obj_ext
+	test_out=$test$out_ext
+	$compiler $test$test_ext > $test_obj
+	$runtime $test_obj > $test_out
+    done
+fi
