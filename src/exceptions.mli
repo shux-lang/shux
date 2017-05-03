9c31d951bcaac5eded1cdd41095c1786eff34096
j-hui <j.hui@columbia.edu>
2017-05-03 19:26:19 -0400

    okay i really don't know how to write exceptions in OCaml

--- src/exceptions.mli
+++ src/exceptions.mli
@@ -1,14 +1,12 @@
 open Ast
-open Sast
 
-exception BinopTypErr of { ltyp: typ; binop: bin_op; rtyp: typ }
-exception UnopTypErr of { unop: un_op; utyp: typ }
-exception AccessErr of { id: string; struct_typ: typ; field: string }
-exception CondTypErr of { if_typ: typ; then_typ: typ; else_typ: typ }
+exception BinopTypErr of (typ * bin_op * typ)
+exception UnopTypErr of (un_op * typ)
+exception AccessErr of (string * typ * string)
+exception CondTypErr of (typ * typ * typ)
 exception UndeclaredId of string
 exception NameConflict of string
-exception GnCallErr of string
-exception FnCallTypErr of { fn_id: string; fn_ret_typ: typ; (* expected type *) exp_typ: typ }
-exception FnArgArr of { fn_id string; fn_formal_typ: typ; fn_actual_typ: typ }
-exception ForbiddenBindTypErr of { id: string; bind_typ: typ }
-exception InternalErr of { why: string; ctx: string } (* flexible generic illegal internal funsies *)
+exception GnCallErr of string 
+exception FnCallTypErr of (string * (* actual *) typ * (* expected *) typ)
+exception FnArgArr of (string * typ * typ)
+exception ForbiddenBindTypErr of (string * typ )
