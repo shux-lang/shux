0faee15244ed31e1a99adb3adbd3ffbd2ea353bb
j-hui <j.hui@columbia.edu>
2017-05-03 19:33:28 -0400

    'self-documenting' code

--- src/exceptions.mli
+++ src/exceptions.mli
@@ -1,12 +1,13 @@
 open Ast
 
-exception BinopTypErr of (typ * bin_op * typ)
+type id_err = string
+exception BinopTypErr of ((* left *) typ * bin_op * (* right *) typ)
 exception UnopTypErr of (un_op * typ)
-exception AccessErr of (string * typ * string)
-exception CondTypErr of (typ * typ * typ)
-exception UndeclaredId of string
-exception NameConflict of string
-exception GnCallErr of string 
-exception FnCallTypErr of (string * (* actual *) typ * (* expected *) typ)
-exception FnArgArr of (string * typ * typ)
-exception ForbiddenBindTypErr of (string * typ )
+exception AccessErr of ((* struct *) id_err * (* member *) typ *  (* member *) id_err)
+exception CondTypErr of ((* if *) typ * (* then *) typ * (* else *) typ)
+exception UndeclaredId of id_err
+exception NameConflict of id_err
+exception GnCallErr of id_err 
+exception FnCallTypErr of ((* fn *) id_err * (* return *) typ * (* expected *) typ)
+exception FnArgArr of ((* fn *) id_err * (* actual *) typ * (* formal *) typ)
+exception ForbiddenBindTypErr of (id_err * typ)
